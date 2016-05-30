36822780dd35339376ed173512764ce7e2e951eb
Oleg Semyonov <os-github-com@os-propo.info>
2016-05-31 00:31:24 +0300

    Make webapp window title and header customizable

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Make miner data accessible to the router
 app.use(function(req, res, next) {
+    req.title = config.title;
     req.json = {
         "miners" : miners.json,
         "refresh" : config.web_refresh
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // GET home page
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Miner Info' });
+    res.render('index', { title: req.title });
 });
 
 module.exports = router;
