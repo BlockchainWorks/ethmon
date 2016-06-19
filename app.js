94bf005fad5c8605d2246812124f07270e270330
Oleg Semyonov <os-github-com@os-propo.info>
2016-06-19 18:41:26 +0300

    Remove unused modules provide by express-generator by default

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var miners = require('./routes/miners');
@@ -16,10 +13,6 @@ app.set('view engine', 'jade');
 
 // Uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Make miner data accessible to the router
